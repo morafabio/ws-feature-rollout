4fc8f9c8c907b1e833b2634caae844a7777c48cb
famora <famora@ebay.com>
2017-06-21 18:21:15 +0200

    step 0

--- app/src/controllers.php
+++ app/src/controllers.php
@@ -8,39 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app->get('/', function (Request $request) use ($app) {
 
-    $extraHeaders = [];
-    $redisConfig = ['host' => 'redis'];
-    $redis = new \Predis\Client($redisConfig);
-    $featureService = new \Features\Service($redis);
-
-    // Switch
-    $feature = new \Features\SwitchFeature('featureFacebook');
-
-    // Condition
-    $conditionFn = function() use ($request) { return (bool) $request->get('featureFacebook'); };
-//    $feature = new \Features\ConditionFeature('featureFacebook', $conditionFn);
-
-    // Percentage
-//    $feature = new \Features\PercentageFeature('featureFacebook');
-//    $feature->initialize($app['session']);
-//    $extraHeaders = ['X-Feature-UserQuota' => $feature->getUserQuota()];
-
-    $facebookLanding = $featureService->isActive($feature);
-
-    $sender = new \Liuggio\StatsdClient\Sender\SocketSender(/*'localhost', 8126, 'udp'*/);
-    $client  = new \Liuggio\StatsdClient\StatsdClient($sender);
-    $factory = new \Liuggio\StatsdClient\Factory\StatsdDataFactory('\Liuggio\StatsdClient\Entity\StatsdData');
-
-    $statsd = new \Liuggio\StatsdClient\Service\StatsdService($client, $factory);
-
-    $statsdKey = sprintf('stats.web.registration.%s.count', $facebookLanding ? 'social' : 'master');
-    $statsd->increment($statsdKey);
-
-    $app['featureFacebook'] = $facebookLanding;
-
     return new Response(
-        $app['twig']->render('index.html.twig', []), 200,
-        ['X-Feature' => $facebookLanding ? 'feature-facebook' : 'master'] + $extraHeaders
+        $app['twig']->render('index.html.twig', []), 200
     );
 
 })->bind('homepage');
