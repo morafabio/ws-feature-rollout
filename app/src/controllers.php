d342549caba9039569b0bcda8e2b3f6cc3f77d2d
famora <famora@ebay.com>
2017-06-21 18:22:40 +0200

    step 1

--- app/src/controllers.php
+++ app/src/controllers.php
@@ -8,8 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app->get('/', function (Request $request) use ($app) {
 
+    $facebookLanding = true;
+    $app['featureFacebook'] = $facebookLanding;
+
     return new Response(
-        $app['twig']->render('index.html.twig', []), 200
+        $app['twig']->render('index.html.twig', []), 200,
+        ['X-Feature' => $facebookLanding ? 'feature-facebook' : 'master']
     );
 
 })->bind('homepage');
